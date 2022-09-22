69c6ca205906f0554fcf90b1ece7679349362a15
Nick <efutin.kolya@yandex.ru>
2022-09-22 10:27:54 +0300

    дз 8

---
+++ Program8.cs
@@ -0,0 +1,12 @@
+﻿
+        int n = 1;
+        Console.Write("До какого числа тебе нужны четные числа?  ");
+
+        int m = int.Parse(Console.ReadLine());
+        for (int i = n; i <= m; i++)
+        {
+            if (i % 2 == 0) 
+            {
+                Console.Write(i + " ");
+            }
+        }
